d023a5201a03635ed0032132a93645296ccc8ee9
IgnacioSan22 <iceamanos1998@gmail.com>
2021-10-19 15:46:11 +0200

    Fixed mixing gradients

--- Week2/Copy_2_of_start_mixing_gradient.m
+++ Week2/Copy_2_of_start_mixing_gradient.m
@@ -1,6 +1,8 @@
 clearvars;
-dst = double(imread('Images/rainbow_dst.png'));
-src = double(imread('Images/rainbow_src.png'));
+% dst = double(imread('Images/rainbow_dst.png'));
+% src = double(imread('Images/rainbow_src.png'));
+dst = double(imread('Images/lena.png'));
+src = double(imread('Images/girl.png'));
 [ni,nj, nChannels]=size(dst);
 
 param.hi=1;
@@ -8,8 +10,10 @@ param.hj=1;
 
 
 %masks to exchange: Eyes
-mask_src=logical(imread('Images/rainbow_src_mask.png'));
-mask_dst=logical(imread('Images/rainbow_dst_mask.png'));
+% mask_src=logical(imread('Images/rainbow_src_mask.png'));
+% mask_dst=logical(imread('Images/rainbow_dst_mask.png'));
+mask_src=logical(imread('Images/mask_src_mouth.png'));
+mask_dst=logical(imread('Images/mask_dst_mouth.png'));
 
 for nC = 1: nChannels
     
@@ -21,6 +25,7 @@ for nC = 1: nChannels
     driving_Src_Grad_i(mask_dst(:)) = Src_Grad_i(mask_src(:));
     driving_Src_Grad_j = zeros(size(src(:,:,1)));
     driving_Src_Grad_j(mask_dst(:)) = Src_Grad_j(mask_src(:));
+    magnitudeSrc = sqrt(driving_Src_Grad_i.^2 + driving_Src_Grad_j.^2);
 
     Dst_Grad_i = G3_DiFwd(dst(:,:,nC), param.hi);
     Dst_Grad_j = G3_DjFwd(dst(:,:,nC), param.hj);
@@ -29,13 +34,14 @@ for nC = 1: nChannels
     driving_Dst_Grad_i(mask_dst(:)) = Dst_Grad_i(mask_dst(:));
     driving_Dst_Grad_j = zeros(size(src(:,:,1)));
     driving_Dst_Grad_j(mask_dst(:)) = Dst_Grad_j(mask_dst(:));
+    magnitudeDst = sqrt(driving_Dst_Grad_i.^2 + driving_Dst_Grad_j.^2);
     
-    driving_Src_Grad_i(abs(driving_Dst_Grad_i) > abs(driving_Src_Grad_i)) = driving_Dst_Grad_i(abs(driving_Dst_Grad_i) > abs(driving_Src_Grad_i));
-    driving_Src_Grad_j(abs(driving_Dst_Grad_j) > abs(driving_Src_Grad_j)) = driving_Dst_Grad_i(abs(driving_Dst_Grad_j) > abs(driving_Src_Grad_j));    
+    driving_Src_Grad_i(magnitudeDst > magnitudeSrc) = driving_Dst_Grad_i(magnitudeDst > magnitudeSrc);
+    driving_Src_Grad_j(magnitudeDst > magnitudeSrc) = driving_Dst_Grad_j(magnitudeDst > magnitudeSrc);    
     
     driving_on_dst = zeros(size(src(:,:,1))); 
 
-    driving_on_dst = -G3_DiBwd(driving_Src_Grad_i(:,:), param.hi) - G3_DjBwd(driving_Src_Grad_j(:,:), param.hj);
+    driving_on_dst = - G3_DiBwd(driving_Src_Grad_i(:,:), param.hi) - G3_DjBwd(driving_Src_Grad_j(:,:), param.hj);
     param.driving = driving_on_dst;
 
     dst1(:,:,nC) = G3_Poisson_Equation_Axb(dst(:,:,nC), mask_dst,  param);
